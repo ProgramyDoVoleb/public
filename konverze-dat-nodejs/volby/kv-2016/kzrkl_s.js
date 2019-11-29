6bb7f8a88c309cc6c9bd36ad62a5b0113ebf7fbb
Štěpán Ryšavý <omicrone@gmail.com>
2019-11-29 15:01:26 +0100

    Úklid

--- konverze-dat-nodejs/volby/kv-2016/kzrkl_s.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs'),
-    iconv = require('iconv-lite');
-
-fs.createReadStream('../zdroje/volby/kv-2016/kzrkl_s.xml')
-    .pipe(iconv.decodeStream('win1250'))
-    .pipe(iconv.encodeStream('utf8'))
-    .pipe(fs.createWriteStream('../zdroje/volby/kv-2016/kzrkl_s-utf8.xml'));
