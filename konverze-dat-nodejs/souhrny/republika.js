d994e680f981f6ecd802f18e61e19e92b3ed7904
Štěpán Ryšavý <omicrone@gmail.com>
2019-12-07 23:05:13 +0100

    Souhrn za republiku

---
+++ konverze-dat-nodejs/souhrny/republika.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+
+function writeJSON (json, file) {
+  fs.writeFile(file, JSON.stringify(json), function(err) {
+      if(err) {
+          return console.log(err);
+      }
+  });
+}
+
+var hierarchyFile = new Promise (function (resolve, reject) {
+  fs.readFile('../data/obecne/obce-struktura.json', function(err, content) {
+    resolve(JSON.parse(content));
+  });
+});
+
+function processType(years, path, o) {
+  var results = [];
+
+  years.forEach(year => {
+    results.push({
+      year: year,
+      data: JSON.parse(fs.readFileSync('../data/volby/' + path + '/' + year + '/vysledky.json'))
+    })
+  })
+
+  results.forEach(election => {
+    var el = election.data;
+        el.year = election.year;
+
+    o.push(el);
+  });
+}
+
+function processFile(o) {
+  try {
+    processType([2013, 2018], "p", o.volby.prezident);
+    processType([2017], "psp", o.volby.snemovna);
+    processType([2019], "ep", o.volby.eu);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+Promise.all([hierarchyFile]).then(values => {
+  var o = {
+    num: 0,
+    name: "Česká republika",
+    nuts: "CZ01",
+    volby: {
+      prezident: [],
+      snemovna: [],
+      eu: []
+    }
+  };
+
+  processFile(o);
+
+  writeJSON(o, '../data/souhrny/republika/souhrn.json');
+});
