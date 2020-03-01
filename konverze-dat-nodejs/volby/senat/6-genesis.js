6906b3630b0c99144e7a462fdfdbe529069c8a45
Štěpán Ryšavý <git@programydovoleb.cz>
2020-03-01 11:53:10 +0100

    Genesis generuje změny pro jednotlivé strany

--- konverze-dat-nodejs/volby/senat/6-genesis.js
+++ konverze-dat-nodejs/volby/senat/6-genesis.js
@@ -12,14 +12,16 @@ var genesis = {
   senate: []
 }
 
+var senate = [];
+
 for (var i = 0; i < 81; i++) {
-  genesis.senate.push({
+  senate.push({
     area: i + 1,
     elected: undefined
   })
 }
 
-list.forEach(date => {
+list.forEach((date, dateIndex) => {
   var el = elections.find(e => e.id === date);
   var result = JSON.parse(fs.readFileSync(targetDIR + date + '/vysledky.json'));
 
@@ -42,17 +44,56 @@ list.forEach(date => {
 
       obj.elected.push(item);
 
-      genesis.senate[area.id - 1].elected = area.winner;
+      senate[area.id - 1].elected = area.winner;
 
     });
 
-    fs.writeFileSync(targetDIR + '/' + date + '/senate.json', JSON.stringify(genesis.senate));
-    fs.chmodSync(targetDIR + '/' + date + '/senate.json', 0x755)
+    fs.writeFileSync(targetDIR + date + '/senate.json', JSON.stringify(senate));
+
+    var parties = [];
+
+    senate.forEach(senator => {
+
+      var member = parties.find(p => p.reg === senator.elected.member);
+
+      if (!member) {
+        parties.push({reg: senator.elected.member, members: 0, nominees: 0});
+        member = parties.find(p => p.reg === senator.elected.member);
+      }
+
+      var nominee = parties.find(p => p.reg === senator.elected.nominee);
+
+      if (!nominee) {
+        parties.push({reg: senator.elected.nominee, members: 0, nominees: 0});
+        nominee = parties.find(p => p.reg === senator.elected.nominee);
+      }
+
+      member.members++;
+      nominee.nominees++;
+    });
+
+    if (dateIndex > 0) {
+      parties.forEach(party => {
+        var partyPrevious = genesis.senate[dateIndex - 1].parties.find(p => p.reg === party.reg);
+
+        if (partyPrevious) {
+          party.diff = {
+            members: party.members - partyPrevious.members,
+            nominees: party.nominees - partyPrevious.nominees
+          }
+        } else {
+          party.diff = {
+            members: party.members,
+            nominees: party.nominees
+          }
+        }
+      });
+    }
+
+    genesis.senate.push({date, parties});
 
     genesis.timeline.push(obj);
   }
 });
-
-genesis.senate = undefined;
 
 fs.writeFile(targetDIR + 'genesis.json', JSON.stringify(genesis), () => {});
