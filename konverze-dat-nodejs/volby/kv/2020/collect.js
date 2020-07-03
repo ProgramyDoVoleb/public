df3c2be8ccf0f36119300bc54f90c7cc3512a448
Štěpán Ryšavý <git@programydovoleb.cz>
2020-07-03 16:42:07 +0200

    PLK Piráti

--- konverze-dat-nodejs/volby/kv/2020/collect.js
+++ konverze-dat-nodejs/volby/kv/2020/collect.js
@@ -16,11 +16,11 @@ fs.writeFileSync(dir + 'strany.json', JSON.stringify(json, null, 2));
 
 var cmsg = process.argv[2] || "Aktualizace výpisu stran";
 
-setTimeout(() => {
-  execSync('find ../data/obecne/strany/loga -type f -exec chmod 0755 {} \\;',{stdio: 'inherit'});
-  execSync('find ../data/lide/fotky -type f -exec chmod 0755 {} \\;',{stdio: 'inherit'});
-  execSync('find ../data/volby/kv/2020/data -type f -exec chmod 0755 {} \\;',{stdio: 'inherit'});
-}, 500);
-setTimeout(() => execSync('git add --all',{stdio: 'inherit'}), 4000);
-setTimeout(() => execSync('git commit -a -m "' + cmsg + '"',{stdio: 'inherit'}), 4500);
-setTimeout(() => execSync('git ftp push',{stdio: 'inherit'}), 5000);
+// setTimeout(() => {
+//   execSync('find ../data/obecne/strany/loga -type f -exec chmod 0755 {} \\;',{stdio: 'inherit'});
+//   execSync('find ../data/lide/fotky -type f -exec chmod 0755 {} \\;',{stdio: 'inherit'});
+//   execSync('find ../data/volby/kv/2020/data -type f -exec chmod 0755 {} \\;',{stdio: 'inherit'});
+// }, 500);
+setTimeout(() => execSync('git add --all',{stdio: 'inherit'}), 1000);
+setTimeout(() => execSync('git commit -a -m "' + cmsg + '"',{stdio: 'inherit'}), 1500);
+setTimeout(() => execSync('git ftp push',{stdio: 'inherit'}), 2000);
