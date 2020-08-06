349e7fa9f411c23a4aa9992e4b79bf3ea039ff01
Štěpán Ryšavý <git@programydovoleb.cz>
2020-08-06 18:20:28 +0200

    Piráti

--- konverze-dat-nodejs/volby/kv/2020/inject.js
+++ konverze-dat-nodejs/volby/kv/2020/inject.js
@@ -1,9 +1,16 @@
 var fs = require('fs');
 
-var link = './data/volby/kv/2020/data/' + process.argv[2];
+var link = './data/volby/kv/2020/data/' + process.argv[2] + '.json';
+var data = process.argv[3];
 
 var party = JSON.parse(fs.readFileSync(link));
 
+var json = JSON.parse(data);
 
+json.forEach((p, i) => {
+  party.list[i].age = p.age;
+  party.list[i].work = p.work;
+  party.list[i].home = p.home;
+});
 
 fs.writeFileSync(link, JSON.stringify(party, null, 2));
