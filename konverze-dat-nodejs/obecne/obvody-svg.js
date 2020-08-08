0471671625d29a4b1fad8f85ac07befcf6618ee5
Štěpán Ryšavý <git@programydovoleb.cz>
2020-08-08 21:12:27 +0200

    svg

--- konverze-dat-nodejs/obecne/obvody-svg.js
+++ konverze-dat-nodejs/obecne/obvody-svg.js
@@ -5,16 +5,15 @@ var json = JSON.parse(fs.readFileSync('data/obecne/senatni-obvody-svg.json'))
 json.forEach((o, i) => {
   if (i > 80) return;
 
-  var f = 'data/obecne/obce-v-obvodech/' + (i + 1) + '.json';
-  var ff = fs.readFileSync(f);
+  var f = 'data/obecne/obce-v-obvodech/' + (i + 1);
+  var ff = fs.readFileSync(f + '.json');
 
   if (ff) {
     var obv = JSON.parse(ff)
 
-    obv.svg = o;
+    obv.svg = null;
 
-    fs.writeFileSync(f, JSON.stringify(obv, null, 2));
-
-    console.log(obv.id);
+    fs.writeFileSync(f + '.json', JSON.stringify(obv, null, 2));
+    fs.writeFileSync('data/obecne/info/obvody/' + (i + 1) + '.svg', '<svg viewbox="0 0 500 600"><path d="' + o + '" /></svg>');
   }
 });
