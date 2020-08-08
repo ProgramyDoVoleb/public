7ca5be791b713b569a8082e5d34528e95886b8f2
Štěpán Ryšavý <git@programydovoleb.cz>
2020-08-08 22:32:23 +0200

    svg

--- konverze-dat-nodejs/obecne/obvody-svg.js
+++ konverze-dat-nodejs/obecne/obvody-svg.js
@@ -13,13 +13,18 @@ json.forEach((o, i) => {
 
     obv.svg = [[15, 50], [16, 51]];
 
-    var c = []
+    var c = [
+      Math.round(o.bb[0]) - 10000,
+      Math.round(o.bb[1]) - 10000,
+      Math.round(o.bb[2]) + 10000,
+      Math.round(o.bb[3]) + 10000,
+    ]
 
-    o.bb.forEach(b => {
-      c.push(Math.round(Number(b)));
-    })
+    // o.bb.forEach(b => {
+    //   c.push(Math.round(Number(b)));
+    // })
 
     fs.writeFileSync(f + '.json', JSON.stringify(obv, null, 2));
-    fs.writeFileSync('data/obecne/info/obvody/' + (i + 1) + '.svg', '<svg viewBox="' + c.join(' ') + '" xmlns="http://www.w3.org/2000/svg"><mask id="mm"><rect x="-10000" y="-10000" width="30000" height="30000" fill="#fff" /><path d="' + o.d + '" fill="#000" /></mask><rect x="-10000" y="-10000" width="30000" height="30000" fill="#fff" opacity="1" mask="url(#mm)" /></svg>');
+    fs.writeFileSync('data/obecne/info/obvody/' + (i + 1) + '.svg', '<svg viewBox="' + c.join(' ') + '" xmlns="http://www.w3.org/2000/svg"><mask id="mm"><rect x="-10000" y="-10000" width="30000" height="30000" fill="#fff" /><path d="' + o.d + '" fill="#000" /></mask><rect x="-10000" y="-10000" width="30000" height="30000" fill="#fff" opacity=".3" mask="url(#mm)" /></svg>');
   }
 });
